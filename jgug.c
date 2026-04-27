c8dd944f3eb4a88edddfa0064736c898fa53a23e
nerdybag2007 <gursimar089@gmail.com>
2026-04-27 11:36:55 +0530

    tft

---
+++ jgug.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main(){
+    int a, b, c, n;
+    printf("enter a number");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n; j++)
+        {
+        

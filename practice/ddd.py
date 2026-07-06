922a62eefee2a66a0a110ecb65ec65d6c6e1f6dc
nerdybag2007 <gursimar089@gmail.com>
2026-07-06 08:53:33 +0000

    Jul 6, 2026, 2:23 PM

---
+++ practice/ddd.py
@@ -0,0 +1,9 @@
+# wap a program to ask user movies
+list=[]
+m1=input("enter your fav movies")
+m2=input("enter your fav movies")
+m3=input("enter your fav movies")
+list.append(m1)
+list.append(m3)
+list.append(m2)
+print(list)

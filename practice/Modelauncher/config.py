ff5d3abfe52b3de0d08f130c44360790821edf1a
nerdybag2007 <gursimar089@gmail.com>
2026-07-06 09:10:35 +0000

    Jul 6, 2026, 2:40 PM

--- practice/Modelauncher/config.py
+++ practice/Modelauncher/config.py
@@ -1,2 +1,13 @@
-import customtkinter
-customtkinter.dark
+import customtkinter as ctk
+
+# Appearance
+ctk.set_appearance_mode("Dark")      # Options: "Dark", "Light", "System"
+ctk.set_default_color_theme("blue")  # Theme color
+
+# Create window
+app = ctk.CTk()
+app.title("Mode Launcher")
+app.geometry("500x400")
+
+# Keep window running
+app.mainloop()

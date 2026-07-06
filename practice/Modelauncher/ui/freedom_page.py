f72612f44751ed705030ff6d729ccdd83fc07de7
nerdybag2007 <gursimar089@gmail.com>
2026-07-06 17:42:29 +0000

    Jul 6, 2026, 11:12 PM

--- practice/Modelauncher/ui/freedom_page.py
+++
@@ -1,9 +0,0 @@
-from config import WORKSPACES
-from ui.workspace_page import WorkspacePage
-
-
-class FreedomPage(WorkspacePage):
-    """Freedom workspace: placeholder until YouTube/Spotify/etc. are added to config."""
-
-    def __init__(self, master, on_back, **kwargs):
-        super().__init__(master, WORKSPACES["freedom"], on_back, **kwargs)
--- practice/Modelauncher/ui/gaming_page.py
+++
@@ -1,9 +0,0 @@
-from config import WORKSPACES
-from ui.workspace_page import WorkspacePage
-
-
-class GamingPage(WorkspacePage):
-    """Gaming workspace: placeholder until Steam/Epic/Discord etc. are added to config."""
-
-    def __init__(self, master, on_back, **kwargs):
-        super().__init__(master, WORKSPACES["gaming"], on_back, **kwargs)

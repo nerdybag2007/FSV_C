bb5c6f20614f48e86430f1ff6f791983a76ee057
nerdybag2007 <gursimar089@gmail.com>
2026-07-06 18:07:41 +0000

    Jul 6, 2026, 11:37 PM

--- practice/missing.py
+++ practice/missing.py
@@ -2,5 +2,15 @@ list=[1,2,3,4,5]
 n=int(input("enter your number"))
 for i in range(len(list)):
     for j in range(i+1):
-        list.append
+        missing={
+            missing[1]:1,
+            missing[2]:2,
+            missing[3]:3,
+            missing[4]:4,
+            missing[5]:5,
+        }
+        if 5 in missing:
+            print("hello")
+        else:
+            print("no")
 

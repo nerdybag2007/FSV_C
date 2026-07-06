246755b660eb9ee3701026d974336d3dfcfa4d62
nerdybag2007 <gursimar089@gmail.com>
2026-07-06 18:21:13 +0000

    Jul 6, 2026, 11:51 PM

--- practice/missing.py
+++ practice/missing.py
@@ -3,7 +3,8 @@ n=int(input("enter your number"))
 for i in range(len(list)-1):
     for j in range(i+1,len(list)):
         if (list[i]+list[j]==n):
-    print(i,j)
+            print(i)
+            print(j)
 
         
 

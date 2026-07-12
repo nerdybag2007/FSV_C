d160220f8bbf9e264b6911c676bcafd9831fd75e
nerdybag2007 <gursimar089@gmail.com>
2026-07-12 17:44:00 +0000

    Jul 12, 2026, 11:14 PM

--- student managemetn system/main.py
+++ student managemetn system/main.py
@@ -1,16 +1,22 @@
-while True:
-    print("add student")
-    print("view student")
-    print("search student")
-    print("update student")
-    print("delete student")
-    print("exit student")
-    choice=int(input("enter your choice:"))
-    if choice==6:
-        print("thankyou for choosing studnet managemnet system")
-        break
-    elif 1<= choice<=5:
-        print("feature coming soon")
-    else:
-        print("invalid choice pleasse try again")
+def starter():
+    try:
+        while True:
+            print("add student")
+            print("view student")
+            print("search student")
+            print("update student")
+            print("delete student")
+            print("exit student")
+            choice=int(input("enter your choice:"))
+            if choice==6:
+                print("thankyou for choosing studnet managemnet system")
+                break
+            elif 1<= choice<=5:
+                print("feature coming soon")
+            else:
+                print("invalid choice pleasse try again")
+    except ValueError:
+        print("error what the hell")
+starter()
+        
 

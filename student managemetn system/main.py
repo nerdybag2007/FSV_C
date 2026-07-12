a6348504a174694df46affcdda879d0593d10f9a
nerdybag2007 <gursimar089@gmail.com>
2026-07-12 17:32:02 +0000

    Jul 12, 2026, 11:02 PM

--- student managemetn system/main.py
+++ student managemetn system/main.py
@@ -1,15 +1,15 @@
 n=int(input("please enter your number"))
-1=("add student")
-2=("view student")
-3=("search student")
-4=("update student")
-5=("update student")
+1==("add student")
+2==("view student")
+3==("search student")
+4==("update student")
+5==("update student")
 6=("update student")
 while n>0:
     if n==(6):
         print("thankyou for choosing student management system")
         break
-    elif(n<=5 or n>=1):
+    elif(n<=5 and n>=1):
         print("feature coming soon")
         break
     elif(n<1 or n>6):

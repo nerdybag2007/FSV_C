7f02d8b2b92e09354bcd76324b9cfa32cafc593d
nerdybag2007 <gursimar089@gmail.com>
2026-07-12 17:24:55 +0000

    Jul 12, 2026, 10:54 PM

--- student managemetn system/main.py
+++ student managemetn system/main.py
@@ -1,6 +1,10 @@
 n=int(input("please enter your number"))
 1=("add student")
-
+2=("view student")
+3=("search student")
+4=("update student")
+5=("update student")
+6=("update student")
 while n>0:
     if n==(6):
         print("thankyou for choosing student management system")

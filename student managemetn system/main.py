91253f083850cb94d73ac1ae81280970a4fbcd20
nerdybag2007 <gursimar089@gmail.com>
2026-07-12 17:20:02 +0000

    Jul 12, 2026, 10:50 PM

--- student managemetn system/main.py
+++ student managemetn system/main.py
@@ -3,6 +3,9 @@ while n>0:
     if n==(6):
         print("thankyou for choosing student management system")
         break
-    elif(1>=n<=5):
+    elif(n<=5 or n>=1):
         print("feature coming soon")
         break
+    elif(n<1 or n>6):
+        print("invalid choice! please try again later")
+        

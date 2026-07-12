16ccd297d0946969794d9b7f4d5fe4599792fe80
nerdybag2007 <gursimar089@gmail.com>
2026-07-12 17:18:13 +0000

    Jul 12, 2026, 10:48 PM

---
+++ student managemetn system/main.py
@@ -0,0 +1,8 @@
+n=int(input("please enter your number"))
+while n>0:
+    if n==(6):
+        print("thankyou for choosing student management system")
+        break
+    elif(1>=n<=5):
+        print("feature coming soon")
+        break
